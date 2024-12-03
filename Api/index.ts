2378d5ba86b8aaff8343e5240e376111941ced37
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-12-03 19:20:40 +0600

    [API] Create 2 post request on encode and decode

---
+++ Api/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const Vigenere = require('caesar-salad').Vigenere;
+const app = express();
+const port = 8000;
+app.use(express.json());
+
+const run = async () => {
+    app.post(`/encode`, (req, res) => {
+        res.send({ "encoded":Vigenere.Cipher(`${req.body.password}`).crypt(`${ req.body.message}`)});
+    });
+
+    app.post(`/decode`, (req, res) => {
+       res.send({ "decoded":Vigenere.Decipher(`${req.body.password}`).crypt(`${ req.body.message}`)});
+    });
+    app.listen(port, () => {
+        console.log(`Server started on http://localhost:${port} port!`);
+    })
+};
+
+run().catch(console.error);
+
