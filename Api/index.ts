da3983438346edbaf2ea640c7e0cb2ad03d747b1
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-12-04 19:33:55 +0600

    [API] Refactor code

--- Api/index.ts
+++ Api/index.ts
@@ -9,12 +9,15 @@ app.use(express.json());
 
 const run = async () => {
     app.post(`/encode`, (req, res) => {
-        res.send({ "encoded":Vigenere.Cipher(`${req.body.password}`).crypt(`${ req.body.message}`)});
+        console.log(req.body.password);
+        console.log(req.body.encoded);
+        res.send({ "encoded":Vigenere.Cipher(`${req.body.password}`).crypt(`${ req.body.encoded}`) });
     });
 
     app.post(`/decode`, (req, res) => {
-       res.send({ "decoded":Vigenere.Decipher(`${req.body.password}`).crypt(`${ req.body.message}`)});
+       res.send({ "decoded":Vigenere.Decipher(`${req.body.password}`).crypt(`${ req.body.decoded}`)});
     });
+
     app.listen(port, () => {
         console.log(`Server started on http://localhost:${port} port!`);
     })
